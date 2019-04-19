afdc5c21fe7c58af805aa7bf74e882475bc430c9
John Whiles <johnwhiles@outlook.com>
2019-04-19 11:42:19 +0100

    Add keyboard controls
    pressing space advances the emojis.

--- main.js
+++ main.js
@@ -10,10 +10,32 @@ const setContent = (element, content) => (element.innerText = content);
 const emojiOne = document.getElementById('first');
 const emojiTwo = document.getElementById('second');
 
-setContent(emojiOne, getRandomElement(emojis));
-setContent(emojiTwo, getRandomElement(emojis));
-
-const interval = setInterval(() => {
+updateEmojis = () => {
   setContent(emojiOne, getRandomElement(emojis));
   setContent(emojiTwo, getRandomElement(emojis));
-}, 5 * minute);
+}
+
+const space = "Space"
+const enter = "Enter"
+
+const keyboardControls = interval => e => {
+  switch(e.code) {
+    case enter:
+      clearInterval(interval);
+      updateEmojis();
+      interval = setInterval(updateEmojis, 5 * minute);
+      break;
+    default:
+      break;
+  }
+}
+
+const main = () => {
+  updateEmojis();
+
+  let interval = setInterval(updateEmojis, 5 * minute);
+
+  document.addEventListener('keypress', keyboardControls(interval));
+}
+
+main();
